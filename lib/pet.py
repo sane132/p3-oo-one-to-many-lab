3f742deb407614dc754cbc546518c6f59f6da576
Mohamed <muhamadsane114@gmail.com>
2025-08-16 17:44:18 +0300

    passed

--- lib/pet.py
+++ lib/pet.py
@@ -1,18 +1,3 @@
-class Owner:
-    def __init__(self, name):
-        self.name = name
-        
-    def pets(self):
-        return [pet for pet in Pet.all if pet.owner == self]
-    
-    def add_pet(self, pet):
-        if not isinstance(pet, Pet):
-            raise Exception("The pet must be an instance of Pet class")
-        pet.owner = self
-        
-    def get_sorted_pets(self):
-        return sorted(self.pets(), key=lambda pet: pet.name)
-
 class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     all = []
